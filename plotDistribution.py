e25ad652eec6928f216f03925acdba522e38e252
yuyang <huyuu258@gmail.com>
2020-10-17 08:13:21 +0900

    0.15

--- plotDistribution.py
+++ plotDistribution.py
@@ -11,6 +11,8 @@ data = pd.read_csv(path, skiprows=8)
 data.columns = ['r', 'z', 'B']
 data = data.pivot(index='r', columns='z', values='B')
 
+print('variance: {:.5f}'.format(nu.var(data.values)))
+
 pl.contourf(data.index, data.columns, data.values.T, levels=50)
 pl.colorbar()
 pl.show()
